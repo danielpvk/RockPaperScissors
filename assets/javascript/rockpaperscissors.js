f33c7856aab39bed42b5399097b84eae7b73df2c
danielpv <danielpvk@gmail.com>
2018-10-05 23:57:13 -0500

    second try

--- assets/javascript/rockpaperscissors.js
+++ assets/javascript/rockpaperscissors.js
@@ -16,7 +16,38 @@
 
     // Alias database and sub-levels.
     var database = firebase.database();
+    var clickCounter = 0;
+    // Functions
+    // ================================================================================
+    // On Click
+    $("#rock1").on("click", function() {
+        clickCounter++;
+        debugger;
+        // **** Store Click Data to Firebase in a JSON property called clickCount *****
+        // **** Note how we are using the Firebase .set() method ****
+        // **** .ref() refers to the path you want to save your data to
+        // **** Since we left .ref() blank, it will save to the root directory
+        database.ref().set({
+         clickCount: clickCounter
+        });
+    });
+    $('#localplayer').on('click',function(event){
+      
+        var button1="#"+$(event.target).attr("id");
+        var player=event.delegateTarget.childNodes[1].value;
 
+//in this line of code we are deciding if we are player1 or player2 in firebase
+    database.ref().on("value", function(snapshot) {
+    // If Firebase has a player1, the new player will be player2
+        debugger;
+        if (snapshot.child("player1").exists()) {
+        // Set the variables for highBidder/highPrice equal to the stored values.
+        database.ref().set({
+            player2: player
+            });    
+        }
+    });
+    });
     function rockpaperscissors(){
 
         if (stage===0){
